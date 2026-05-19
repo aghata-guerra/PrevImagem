2a411d668828e1d7199bc86e3993dd57e3f113f4
BrunaFrancisca <bruna.franciisca@gmail.com>
2026-05-18 23:08:39 -0300

    adicionando seção de convênios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import ExamesSection from "./components/ExamesSection"
+import Convenios from "./components/Convenios"
 
 function App() {
   return (
     <div>
     <ExamesSection/>
-  
+    <Convenios/>
     
     </div>
   )
