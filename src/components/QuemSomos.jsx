56dbebe906acd1f928d51820a77389d8da92ecdf
Bruno <brunoserpa15@gmail.com>
2026-05-19 22:08:57 -0300

    icones

--- src/components/QuemSomos.jsx
+++ src/components/QuemSomos.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import CardSections from './CardSections';
 
 import fotoClinica from '../assets/previmagem_foto.jpeg';
-import iconeMissao from '../assets/icon-team.png';
-import iconeVisao from '../assets/icon-tech.png';
-import iconeValores from '../assets/icon-user.png';
+import iconeTime from '../assets/icon-team.png';
+import iconeMonitor from '../assets/icon-tech.png';
+import iconeUser from '../assets/icon-user.png';
+import iconeMissao from '../assets/missao.svg';
+import iconeVisao from '../assets/visao.svg';
+import iconeValores from '../assets/valores.svg';
 
 export function QuemSomos() {
   return (
@@ -26,7 +29,7 @@ export function QuemSomos() {
           </p>
 
           <div>
-            <img src={iconeValores} alt="Ícone Atendimento" />
+            <img src={iconeUser} alt="Ícone Atendimento" />
             <div>
               <h3>Atendimento Humanizado</h3>
               <p>Cuidamos de cada paciente com respeito, atenção e empatia</p>
@@ -34,7 +37,7 @@ export function QuemSomos() {
           </div>
 
           <div>
-            <img src={iconeVisao} alt="Ícone Tecnologia" />
+            <img src={iconeMonitor} alt="Ícone Tecnologia" />
             <div>
               <h3>Tecnologia de Ponta</h3>
               <p>Equipamentos modernos para diagnósticos mais precisos e seguros</p>
@@ -42,7 +45,7 @@ export function QuemSomos() {
           </div>
 
           <div>
-            <img src={iconeMissao} alt="Ícone Profissionais" />
+            <img src={iconeTime} alt="Ícone Profissionais" />
             <div>
               <h3>Profissionais Qualificados</h3>
               <p>Equipe especializada e comprometida com sua saúde e bem estar</p>
