6810d6053f4d9cc727cd9f1f61b737a8fa6ec84d
Bruno <brunoserpa15@gmail.com>
2026-05-19 21:34:08 -0300

    Quem Somos

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import EsteticaSection from "./components/EsteticaSection"
 import ExamesSection from "./components/ExamesSection"
 import Medicos from "./components/MedicosSection";
 import Convenios from "./components/Convenios"
+import QuemSomos from './components/QuemSomos';
 
 function App() {
   return (
     <div>
+    <QuemSomos/>
     <ExamesSection/>
     <EsteticaSection/>
     <Medicos/>
